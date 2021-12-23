03d0d3af79072c110a6adb14c2014131643eb068
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-12-22 22:07:37 -0300

    added images and clear project

--- gerenciador-de-projetos/src/App.js
+++ gerenciador-de-projetos/src/App.js
@@ -1,18 +1,7 @@
-import "./App.css";
-
 function App() {
-  const url = "https://via.placeholder.com/200";
-
-  function soma (a, b) {
-    return a + b;
-  }
-
   return (
     <div className="App">
-      <h1>Projeto inicializado!</h1>
-      <p>Gerenciador de Projetos</p>
-      <p>Soma: {soma(10,15)}</p>
-      <img src={url} alt="Minha imagem"/>
+      <h1>Costs</h1>
     </div>
   );
 }
