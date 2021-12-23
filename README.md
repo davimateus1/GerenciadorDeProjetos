707ff7efa62b496766b068ccc1b1828649e57161
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-12-22 21:34:54 -0300

    Created project

--- README.md
+++
@@ -1,1 +0,0 @@
-# GerenciadorDeProjetos
---
+++ gerenciador-de-projetos/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Projeto inicializado!</h1>
+      <p>Gerenciador de Projetos</p>
+    </div>
+  );
+}
+
+export default App;
