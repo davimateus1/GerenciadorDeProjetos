0ef41114f187c5067db68e8dd022138a3b77836c
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-12-22 21:51:01 -0300

    test functions

--- gerenciador-de-projetos/src/App.js
+++ gerenciador-de-projetos/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const url = "https://via.placeholder.com/200";
+
+  function soma (a, b) {
+    return a + b;
+  }
+
   return (
     <div className="App">
       <h1>Projeto inicializado!</h1>
       <p>Gerenciador de Projetos</p>
+      <p>Soma: {soma(10,15)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
