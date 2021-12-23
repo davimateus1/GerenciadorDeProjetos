e4bca0d8e123fa93fa2d4575fcfc8435f2bccf45
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-12-22 22:41:09 -0300

    implement routes and base styles

--- gerenciador-de-projetos/src/App.js
+++ gerenciador-de-projetos/src/App.js
@@ -1,8 +1,38 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Company from "./Components/pages/Company";
+import Contact from "./Components/pages/Contact";
+import Home from "./Components/pages/Home";
+import NewProject from "./Components/pages/NewProject";
+
+import Container from "./Components/layout/Container";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Costs</h1>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Sobre</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
