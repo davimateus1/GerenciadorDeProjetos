e366434d02ca587fbcd876bb91067a768e9728d5
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2022-01-06 12:02:58 -0300

    creating dashboard

--- gerenciador-de-projetos/src/Components/pages/Projects/Projects.js
+++ gerenciador-de-projetos/src/Components/pages/Projects/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../../layout/Message/Message";
+import Container from "../../layout/Container/Container";
+import LinkButton from "../../layout/LinkButton/LinkButton";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
   const location = useLocation();
@@ -10,9 +14,15 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}> 
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
