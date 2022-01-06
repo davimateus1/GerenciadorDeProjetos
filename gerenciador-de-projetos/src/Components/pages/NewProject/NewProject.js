b661af26d94aac32cf1cf9282c3e1730bb459d65
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2022-01-06 11:48:39 -0300

    created system messages

--- gerenciador-de-projetos/src/Components/pages/NewProject/NewProject.js
+++ gerenciador-de-projetos/src/Components/pages/NewProject/NewProject.js
@@ -21,7 +21,7 @@ function NewProject() {
       .then((data) => {
         console.log(data);
         // redirect
-        history.push("/projects", { message: "Projeto criado com sucesso!" });
+        history.push("/projects", { message: "O projeto foi criado com sucesso!" });
       })
       .catch((err) => console.error(err));
   }
